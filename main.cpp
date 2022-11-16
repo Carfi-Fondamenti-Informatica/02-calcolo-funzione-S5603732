f7ba46ed18c836a02ad81ebbf69f5e12e8de9006
S5603732 <118397147+S5603732@users.noreply.github.com>
2022-11-16 22:00:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
+   float a, b, x, y, risultato;
+    cin >> a >> b >> x >> y;
+
+    if ( ((x<0)&&(y>0))) {
+        risultato =  a*x - b*y;
+        cout <<  risultato;
+    }else if (((x>=0) && (y<=0))) {
+        risultato = a*x*x - b*y;
+        cout << risultato;
+    } else {
+        risultato = a*x + b*y*y;
+        cout << risultato;
+    }
    return 0;
 }
